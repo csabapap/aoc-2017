c1cca8f43b6af87fe3364696d873e0300b602902
Csaba Pap <csabapapdev@gmail.com>
2018-11-07 20:04:35 +0100

    day5 part1&2

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -1,11 +1,41 @@
+import kotlin.math.abs
+
 object Day05 {
 
-  fun part1(input: String): Int {
-    TODO()
-  }
+    fun part1(input: String): Int {
+        val numbers = input.split("\n").map { it.toInt() }.toMutableList()
+        var nextIndex = 0
+        var steps = 0
 
-  fun part2(input: String): Int {
-    TODO()
-  }
+        while (nextIndex < numbers.size) {
+            val currentIndex = nextIndex
+            nextIndex = numbers[currentIndex]
+            nextIndex += currentIndex
+            numbers[currentIndex] = numbers[currentIndex] + 1
+            steps +=1
+        }
+
+        return steps
+    }
+
+    fun part2(input: String): Int {
+        val numbers = input.split("\n").map { it.toInt() }.toMutableList()
+        var nextIndex = 0
+        var steps = 0
+
+        while (nextIndex < numbers.size) {
+            val currentIndex = nextIndex
+            val offset = numbers[currentIndex]
+            nextIndex = offset + currentIndex
+            if (offset >= 3) {
+                numbers[currentIndex] = numbers[currentIndex] - 1
+            } else {
+                numbers[currentIndex] = numbers[currentIndex] + 1
+            }
+            steps +=1
+        }
+
+        return steps
+    }
 
 }
